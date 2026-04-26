4c4f747418176886a3b38858bdb9604188aa237f
HebaElgohary <hebasamir535@gmail.com>
2026-04-26 08:50:34 +0200

    feat: add groups Header section

--- app/(drawer)/Groups.tsx
+++ app/(drawer)/Groups.tsx
@@ -1,7 +1,11 @@
+import Header from '@/components/organisms/Header'
 import React from 'react'
+import { View } from 'react-native'
 
 export default function Groups() {
   return (
-    <div>Groups</div>
-  )
+  <View>
+      <Header title='Groups' subtitle='separate your students into groups and share their monthly reports in one time ' btn='Add a Group'/>
+   
+    </View>  )
 }
