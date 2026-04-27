65cbe538ff7c8c293eb687008570c3482b112cba
HebaElgohary <hebasamir535@gmail.com>
2026-04-27 10:52:32 +0200

    feat(schedule): add Header section in schedule page

--- app/(drawer)/Schedule.tsx
+++ app/(drawer)/Schedule.tsx
@@ -1,7 +1,13 @@
+import Header from '@/components/organisms/Header'
 import React from 'react'
+import { View } from 'react-native'
 
 export default function Schedule() {
   return (
-    <div>Schedule</div>
-  )
+<View>
+
+  <Header title='المواعيد' subtitle='' />
+</View>
+
+)
 }
