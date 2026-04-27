e83f7541b9e10bfe1b8bdcc06c1a67e9998f75c7
HebaElgohary <hebasamir535@gmail.com>
2026-04-27 12:14:55 +0200

    style(drawer): add drawer style

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,17 +1,46 @@
 
-import { Drawer } from "expo-router/drawer";
+import { colors } from "@/constants/theme";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Students from "./Students";
+import Schedule from "./Schedule";
+import Sessions from "./Sessions";
+import Groups from "./Groups";
+import MonthlyReports from "./MonthlyReports";
+import Settings from "./Settings";
+import index from ".";
+
 
 export default function Layout() {
+  const Drawer=createDrawerNavigator()
   return (
-    <Drawer   >
-      <Drawer.Screen name="index" options={{ title: "Home page" }} />
-      <Drawer.Screen name="Students" />
-      <Drawer.Screen name="Groups" />
+    <Drawer.Navigator
+    screenOptions={ {
+          sceneStyle:{
+      backgroundColor:colors.secondary
+    },
+    drawerActiveBackgroundColor:colors.secondary,
+    drawerActiveTintColor:colors.btnPrimary,
+    drawerInactiveTintColor:colors.warning,
 
-      <Drawer.Screen name="Sessions" />
-      <Drawer.Screen name="Schedule" />
-      <Drawer.Screen name="Monthly-Reports" />
-      <Drawer.Screen name="Settings" />
-    </Drawer>
+
+    drawerItemStyle:{
+      borderBlockColor:colors.secondary
+
+    },
+        drawerStyle:{
+      backgroundColor:colors.btnPrimary,
+    },
+
+  } }
+    >
+      <Drawer.Screen name="index" options={{ title: "Home page" }} component={index}/>
+      <Drawer.Screen name="Students" component={Students} />
+      <Drawer.Screen name="Groups" component={Groups}/>
+
+      <Drawer.Screen name="Sessions" component={Sessions} />
+      <Drawer.Screen name="Schedule" component={Schedule} />
+      <Drawer.Screen name="Monthly-Reports" component={MonthlyReports} />
+      <Drawer.Screen name="Settings" component={Settings} />
+    </Drawer.Navigator>
   );
 }
