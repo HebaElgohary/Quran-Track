f4365a91025b840993c80313da0fa1bfb427a099
HebaElgohary <hebasamir535@gmail.com>
2026-04-27 18:02:46 +0200

    fix: make checkbox suitable for multible check

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -6,16 +6,38 @@ import React, { useState } from "react";
 import { View } from "react-native";
 
 export default function index() {
-  const [selected,setSelected]=useState(false)
+  const [items, setItems] = useState([
+    {
+      id: 0, label: "heba",checked:false
+    },
+    { id: 1, label: "ahmed" ,checked:false},
+    { id: 2, label: "youmna",checked:false },
+  ]);
   return (
     <View className="flex-1 items-center justify-center">
       <Header
         title="الصفحة الرئيسية"
         subtitle="نظرة عامة على حصصك وطلابك "
       ></Header>
-      <Checkbox label='name' checked={selected} onChange={()=>setSelected(!selected)} />
+     {items.map((item)=> <Checkbox key={item.id}
+        label={item.label}
+        checked={item.checked}
+        onChange={() => {
+          const arr=items.map((i)=>{
+           return i!=item?i:{...i,checked:!i.checked}
+             
+            })
+          setItems(arr )   
+        }}
+      />)}
       <HomeCard title={"عدد الطلاب"} num={2} icon=""></HomeCard>
-      <QuickActions title="اجراءات سريعة" btn1="تقرير حصة جديدة" btn2="ادارة الطلاب" btn3='مواعيد الحصص' btn4='التقارير الشهرية'></QuickActions>
+      <QuickActions
+        title="اجراءات سريعة"
+        btn1="تقرير حصة جديدة"
+        btn2="ادارة الطلاب"
+        btn3="مواعيد الحصص"
+        btn4="التقارير الشهرية"
+      ></QuickActions>
     </View>
   );
 }
