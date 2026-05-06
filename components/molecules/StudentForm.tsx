726de9a4ac4aebb7119d689b7e241674dc633d02
HebaElgohary <hebasamir535@gmail.com>
2026-05-06 18:39:46 +0300

    feat: add studentForm

---
+++ components/molecules/StudentForm.tsx
@@ -0,0 +1,36 @@
+import { getFormFields } from "@/utils/getFormFields";
+import React from "react";
+import {  View } from "react-native";
+import FormField from "../molecules/form/FormField";
+import Button from "../atoms/Button";
+import FormHeading from "./form/FormHeading";
+
+export default function StudentForm() {
+  return (
+    <View style={{backgroundColor:'white',padding:12}}>
+       {/* form heading */} 
+  <FormHeading title='موعد حصة جديدة ' name={'x'}/>
+       {/* /////////////////// */}
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent:'space-between',
+          overflow:'hidden',
+          color:'red'
+        }}
+      >
+        <View style={{display:'flex' ,width:'80%',marginVertical:14}}>
+        {getFormFields("Students")?.map((field) => (
+          <FormField key={field?.label} {...field} />
+        ))}
+        </View>
+        <View style={{display:'flex', flexDirection:'row',justifyContent:'flex-end',gap:5}}>
+        <Button size='md' variant="gray"  textColor="black" >{"الغاء"}</Button>
+       
+        <Button size='md' textColor="white">{"اضافة"}</Button>
+      </View>
+      </form>
+    </View>
+  );
+}
