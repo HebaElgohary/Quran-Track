8f38df066ba862a31b31c223334764efd5a661c1
HebaElgohary <hebasamir535@gmail.com>
2026-05-07 11:29:15 +0300

    fix: transformed getFormName to object

--- components/molecules/form/getFormName.tsx
+++ components/molecules/form/getFormName.tsx
@@ -1,20 +1,11 @@
+import StudentForm from "../StudentForm";
+import GroupsForm from "../GroupForm";
+import SessionForm from "../SessionForm";
+import ScheduleForm from "../ScheduleForm";
 
-import React from 'react'
-import StudentForm  from '../StudentForm';
-import GroupsForm  from '../GroupForm';
-import SessionForm  from '../SessionForm';
-import ScheduleForm from '../ScheduleForm';
-
-export default function (FormName: string,setOpen: any,open:boolean) {
-        switch (FormName) {
-      case "Students":
-        return <StudentForm setOpen={setOpen} open={open}/> ;
-          case "Groups":
-        return <GroupsForm setOpen={setOpen} open={open}/> ;
-          case "Sessions":
-        return <SessionForm setOpen={setOpen} open={open} /> ;
-   case "Schedule":
-        return <ScheduleForm setOpen={setOpen} open={open} /> ;
-
-  
-}}
+export const forms = {
+  Students: StudentForm,
+  Groups: GroupsForm,
+  Sessions: SessionForm,
+  Schedule: ScheduleForm,
+};
