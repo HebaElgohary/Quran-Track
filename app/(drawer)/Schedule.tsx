a04e230485df2a8c2a50dfdeb5cc713cccbb7502
HebaElgohary <hebasamir535@gmail.com>
2026-05-07 18:08:20 +0300

    feat: add NoDataFallback to schedules

--- app/(drawer)/Schedule.tsx
+++ app/(drawer)/Schedule.tsx
@@ -1,15 +1,17 @@
 import ScheduleForm from '@/components/molecules/ScheduleForm'
 import Header from '@/components/organisms/Header'
 import NoDataFallback from '@/components/organisms/NoDataFallback'
+import { Feather } from '@expo/vector-icons'
 import React from 'react'
 import { View } from 'react-native'
 
 export default function Schedule() {
+  const schedule = [];
   return (
 <View style={{direction:'rtl',gap:3}}>
 
   <Header title='المواعيد' subtitle='سينبهك التطبيق بصوت عالى قبل الحصة ب 5 دقائق' btn='اضف موعد جديد' formName='Schedule'/>
-  {/* <NoDataFallback text='لايوجد مواعيد ' btn='اضف اول ميعاد' icon='' /> */}
+  {schedule.length === 0 && <NoDataFallback text='لايوجد مواعيد ' btn='اضف اول ميعاد' Icon={()=><Feather name="calendar" size={30} color="gray" /> } />}
 </View>
 
 )
