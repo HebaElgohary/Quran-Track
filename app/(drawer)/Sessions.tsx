d1995ed7412d68f025b9e4894dc5e5f58c199c88
HebaElgohary <hebasamir535@gmail.com>
2026-05-07 18:01:43 +0300

    feat: add NoDataFallback to session page

--- app/(drawer)/Sessions.tsx
+++ app/(drawer)/Sessions.tsx
@@ -1,11 +1,12 @@
-import Form from '@/components/molecules/form/Form'
 import Filter from '@/components/organisms/Filter'
 import Header from '@/components/organisms/Header'
 import NoDataFallback from '@/components/organisms/NoDataFallback'
+import { Feather } from '@expo/vector-icons'
 import React from 'react'
 import { View } from 'react-native'
 
 export default function sessions() {
+  const sessions = [];
   return (
     <View style={{direction:'rtl'}}>
       <Header title='الحصص' subtitle='كل تقارير الحصص' btn='تقرير حصة جديدة' formName='Sessions' />
@@ -15,7 +16,7 @@ export default function sessions() {
         { label: "محمد", value: "محمد" },
         { label: "عزير", value: "عزير" }, ]} />
       {/* <Form page='Session' btn1='الغاء' btn2='حفظ التقرير'/> */}
-      {/* <NoDataFallback Icon='' text='لاتوجد حصص مسجلة ' btn='اضف اول حصة '/> */}
+    {sessions.length === 0 && <NoDataFallback Icon={() => <Feather name="book-open" size={30} color="gray" />}  text='لاتوجد حصص مسجلة ' btn='اضف اول حصة '/> }
 
     </View>
   )
