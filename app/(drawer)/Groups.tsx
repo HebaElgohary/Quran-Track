c56b1d657693b7c8bfe24bf28003d87a0c9aaae2
HebaElgohary <hebasamir535@gmail.com>
2026-05-18 23:39:02 +0300

    feat: add AddGroup handler in Group component

--- app/(drawer)/Groups.tsx
+++ app/(drawer)/Groups.tsx
@@ -1,13 +1,22 @@
 import Header from "@/components/organisms/Header";
 import NoDataFallback from "@/components/organisms/NoDataFallback";
 import NotificationCard from "@/components/organisms/NotificationsCard";
+import useGroups from "@/hooks/useGroup";
 
 import { Feather } from "@expo/vector-icons";
 import React from "react";
 import { View } from "react-native";
 
 export default function Groups() {
-  const groups = [];
+ const { groups, loading,createGroup } = useGroups();
+
+const AddGroup = async (formData:any) => {
+  const { students, ...groupData } = formData;
+
+  const newGroup = await addGroup(groupData);
+
+  await assignStudentsToGroup(students, newGroup.id);
+};
   return (
     <View style={{ direction: "rtl" }}>
       <Header
@@ -15,6 +24,7 @@ export default function Groups() {
         title="المجموعات"
         subtitle="قسم طلابك لمجموعات وشارك تقاريرهم الشهرية دفعة واحدة  "
         btn="مجموعة جديدة "
+        handleSubmit={AddGroup}
       />
       {groups.length === 0 && (
         <NoDataFallback
