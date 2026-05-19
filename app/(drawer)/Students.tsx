f1a0320c9159a241bbc00283eed313c9070a9be9
HebaElgohary <hebasamir535@gmail.com>
2026-05-19 22:16:57 +0300

    feat: add useToast hook

--- app/(drawer)/Students.tsx
+++ app/(drawer)/Students.tsx
@@ -58,7 +58,7 @@ const confirmDelete = async () => {
         </View>}
   <View style={{gap:10}}>
    {students.map((student) =>
-    <StudentCard key={student.id} updateStudent={editStudent} handleDelete={handleDelete} isStudent student={student} btn1='edit' btn2='delete'></StudentCard>
+    <StudentCard key={student.id} updateStudent={editStudent} handleDelete={confirmDelete} isStudent student={student} btn1='edit' btn2='delete'></StudentCard>
    )}
    </View>
    {/* // will show alert in case of delete only */}
