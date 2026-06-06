4934a3ee9a22f01071d6d4e515520be49cb479d0
HebaElgohary <hebasamir535@gmail.com>
2026-06-06 03:44:12 +0300

    feat: add formSchemas registry

---
+++ utils/formSchemas.ts
@@ -0,0 +1,7 @@
+// import { groupFields } from "@/schemas/groupFields";
+import { studentFields } from "@/schemas/studentFields";
+
+const formSchemas = {
+  Students: studentFields,
+//   Groups: groupFields,
+};
