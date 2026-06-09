a4a5d8cc2413ab77a11205a84f022594584f2374
HebaElgohary <hebasamir535@gmail.com>
2026-06-10 02:01:01 +0300

    feat: add addSession function in sessionStorage

--- storage/sessionStorage.ts
+++ storage/sessionStorage.ts
@@ -1,7 +1,11 @@
-import { Session } from '@/types/appTypes';
+import { Session, SessionFormData } from '@/types/appTypes';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const KEY = 'session';
+
+// =========================
+// GET Sessions
+// =========================
  export const getSessions = async (): Promise<Session[]> => {
   try {
     const data = await AsyncStorage.getItem(KEY);
@@ -12,8 +16,39 @@ const KEY = 'session';
     return [];
   }
 
-  
 };
+  
+// =========================
+// ADD Session
+// =========================
+export const addSession = async (
+  newSession: SessionFormData,
+  studentId: number
+): Promise <Session> => {
+  try {
+    
+    const oldSessions = await getSessions();
+
+
+    const session:Session = {
+      id: Date.now(),
+      ...newSession,
+      studentId:studentId
+    };
+    const updatedSessions = [...oldSessions, session];
+
+    await AsyncStorage.setItem(
+     KEY,
+      JSON.stringify(updatedSessions)
+    );
+    return session;
+  } catch (error) {
+    console.log("Error adding group", error);
+    throw error;
+  }
+};
+
+
 
 
  
