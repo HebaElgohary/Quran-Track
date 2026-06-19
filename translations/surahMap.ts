4912b6df5187b07cfc91c057ac85190a0062393d
HebaElgohary <hebasamir535@gmail.com>
2026-06-19 22:11:06 +0300

    feat: add surahMap in translations

---
+++ translations/surahMap.ts
@@ -0,0 +1,8 @@
+export const surahMap: Record<string, string> = {
+  الفاتحة: "Al-Fatihah",
+  البقرة: "Al-Baqarah",
+  آل_عمران: "Aal-Imran",
+  النساء: "An-Nisa",
+  المائدة: "Al-Ma'idah",
+  الملك: "Al-Mulk",
+};
