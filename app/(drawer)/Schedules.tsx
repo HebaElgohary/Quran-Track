cfd28968017479cc067dfbda430cc04d9d0c1ad0
HebaElgohary <hebasamir535@gmail.com>
2026-06-24 12:03:11 +0300

    fix: updated schedule to remove scheduleNotifications after remove schedule

--- app/(drawer)/Schedules.tsx
+++ app/(drawer)/Schedules.tsx
@@ -11,6 +11,7 @@ import { scheduleSessionNotification } from '@/utils/scheduleSessionNotification
 import { Feather } from '@expo/vector-icons'
 import { useState } from 'react'
 import { View } from 'react-native'
+import * as Notifications from 'expo-notifications'
 
 type AddDataType = ScheduleFormData
 export default function Schedules() {
@@ -39,6 +40,17 @@ await createSchedule({
 // delete student in case of confirm alert //
 const confirmDelete = async () => {
   if (selectedScheduleId===null) return;
+
+  const schedule = schedules.find(
+  s => s.id === selectedScheduleId
+);
+
+if (schedule?.notificationId) {
+  await Notifications.cancelScheduledNotificationAsync(
+    schedule.notificationId
+  );
+}
+
   await removeSchedule(selectedScheduleId);
   showSuccess( 'تم حذف الحصة',
   );
