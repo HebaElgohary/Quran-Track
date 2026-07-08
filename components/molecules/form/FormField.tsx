cbe4fd326a4588a6439c271fc55c41e8f8a43a3a
HebaElgohary <hebasamir535@gmail.com>
2026-07-08 08:16:52 +0300

    feat: add timeInput to formFiled component

--- components/molecules/form/FormField.tsx
+++ components/molecules/form/FormField.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import RadioInput from "../RadioInput";
 import SelectInput from "../SelectInput";
 import DateInput from "../DateInput";
+import TimeInput from "../TimeInput";
 
 export default function FormField(props: any) {
   const { type } = props;
@@ -11,7 +12,7 @@ export default function FormField(props: any) {
 
   if (type == "text"|| type == "password") return <Input {...props} />;
   if (type == "number") return <Input {...props} />;
-
+if (type === "time") return <TimeInput {...props} />;
   if (type == "textarea") return <Input {...props} multiline />;
   if (type == "radio") return <RadioInput {...props} />;
   if (type == "select") return <SelectInput {...props} />;
